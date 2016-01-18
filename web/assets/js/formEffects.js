2eeeacb2d2f47a46b93c1f6cd04072ccfd1f90c6
Badacadabra <baptiste.vannesson@gmail.com>
2016-01-18 19:03:22 +0100

    Intégration du design sous Symfony, épisode I

---
+++ web/assets/js/formEffects.js
@@ -0,0 +1,31 @@
+// Au survol d'une zone de formulaire
+$( ".row.well" ).mouseover(function() {
+    $( this ).prev().css({ "background-color":"#2F88AA", "color":"white" });
+});
+$( ".row.well" ).mouseout(function() {
+    $( this ).prev().css({ "background-color":"#EEEEEE", "color":"gray" });
+});
+
+// Au survol d'un bouton pour sélectionner un fichier
+$( ".btn-file" ).mouseover(function() {
+    $( this ).prev().css({ "background-color":"#2F88AA", "color":"white" });
+});
+$( ".btn-file" ).mouseout(function() {
+    $( this ).prev().css({ "background-color":"#F2F2F2", "color":"gray" });
+});
+
+// Au survol d'un bouton pour afficher le datepicker
+$( ".input-group-addon" ).mouseover(function() {
+    $( this ).parent().parent().prev().css({ "background-color":"#2F88AA", "color":"white" });
+});
+$( ".input-group-addon" ).mouseout(function() {
+    $( this ).parent().parent().prev().css({ "background-color":"#F2F2F2", "color":"gray" });
+});
+
+// Au focus des champs classiques du formulaire
+$( ".form-control" ).focus(function() {
+    $( this ).prev().css({ "background-color":"#2F88AA", "color":"white" });
+});
+$( ".form-control" ).blur(function() {
+    $( this ).prev().css({ "background-color":"#F2F2F2", "color":"gray" });
+});
